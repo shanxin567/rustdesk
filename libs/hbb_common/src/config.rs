d2fdd8cbaf483ac078072109d41d3ad14f1aa241
shanxin567 <58086406+shanxin567@users.noreply.github.com>
2025-01-19 17:31:26 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["xysjct.top"];
-pub const PUBLIC_RS_PUB_KEY: &str = "ECqpRUYBVMNVvxrTfR1m5PWftbnx10PdjErfKdi6Wpw=";
+pub const PUBLIC_RS_PUB_KEY: &str = "AowsoqsYPmPdf00nNnuMW0keQ1s13hHz45DUqeSxe14=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
